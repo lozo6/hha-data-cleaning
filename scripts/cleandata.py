09c93847572cc45423a67cea975b9d4e79e4355b
lozo6 <lorenzo.francisco@stonybrook.edu>
2022-09-05 09:55:50 -0400

    pending final version with clean data

--- scripts/cleandata.py
+++ scripts/cleandata.py
@@ -2,31 +2,51 @@ import pandas as pd
 import datetime as dt
 import uuid
 import numpy as np
+import re
 
 # sets a variable to read csv file in data folder
 learningModels = pd.read_csv('data/School_Learning_Modalities.csv')
-df_learningModels = pd.DataFrame(learningModels)
-print(df_learningModels)
-print('\n')
+# makes it cleaner and efficient to write/read code
+df = learningModels
+
+check = df.sample
 
 # sets a variable to count the number of rows and columns (rows, columns)
-rc_counter = learningModels.shape
-print(f'The number of columns are {rc_counter[0]}.\nThe number of columns are {rc_counter[1]}.\n')
+rc_count = df.shape
+print(f'The number of columns are {rc_count[0]}.\nThe number of columns are {rc_count[1]}.\n')
 
-# print the names of each column in a nice format
-columnNames = list(learningModels)
-for name in columnNames:
-    print(f'"{name}" is in Column {columnNames.index(name) + 1}.')
-print('\n')
+# print the names of each column
+print(list(df))
 
 # cleans up column names
-learningModels.columns = learningModels.columns.str.replace('[^A-Za-z0-9]+', '_')
+df.columns = df.columns.str.replace('[^A-Za-z0-9]+', '_').str.lower().str.strip()
 
-types = learningModels.dtypes
+# prints all dtypes to assess dtypes
+print(df.dtypes)
 
-print(types)
+# only 'Week' needs to be changed from object to datetime
+df['week'] = pd.to_datetime(df['week'])
 
-# cleans up white spaces and special characters
-for name in columnNames:
-    learningModels[name] = learningModels[name].str.strip()
-    learningModels[name] = learningModels[name].str.replace('[^A-Za-z0-9]+', '_')
+# only district_name, learning_modality, week, and city needs to be assessed which are all objects
+# use select_dtypes and .columns to select the columns needed to be assessed
+needsChange = df.select_dtypes(object).columns
+
+# cleans up column values similar to line 22
+df[needsChange] = df[needsChange].apply(lambda x : x.str.replace('[^A-Za-z0-9]+', '_').str.lower().str.strip())
+
+# finds duplicated rows
+df.duplicated()
+# removes duplicated rows
+df.drop_duplicates()
+
+# checks for empty values
+missing = df.isnull().sum()
+df.replace(to_replace='', value=np.nan, inplace=True) ## replacing empty cells with NaN with numpy
+df.replace(to_replace=' ', value=np.nan, inplace=True) ## replacing cells with whitespace with NaN
+
+df['learning_modality'] = df['learning_modality'].apply(lambda x : 'true' if x == 'in_person' else 'false')
+
+# final check to ensure data has been assessed and cleaned up
+print(check)
+
+df.to_csv('data/clean_data/school_learning_modalities_mod.csv')
